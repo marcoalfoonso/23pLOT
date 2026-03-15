1ba635305687365adcf937250ebbd34b1f982d80
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-15 09:02:31 -0600

    yea

--- control23pLOT.js
+++ control23pLOT.js
@@ -1,32 +1,38 @@
-const scene = new THREE.Scene(); //Creating an abject scene
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-); // Creacting an object camera
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-const material = new THREE.LineBasicMaterial({color:0x0000ff})
+document.addEventListener("DOMContentLoaded",()=>{
 
-const points = [];
-points.push(new THREE.Vector3(-10,0,0));
-points.push(new THREE.Vector3(10,7,10));
+    const scene = new THREE.Scene(); //Creating an abject scene
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    ); // Creacting an object camera
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-const line = new THREE.Line(geometry,material);
-scene.add(line);
+    const material = new THREE.LineBasicMaterial({color:0x0000ff})
 
-camera.position.z = 20;
+    const points = [];
+    points.push(new THREE.Vector3(-10,0,0));
+    points.push(new THREE.Vector3(10,7,10));
 
-function animate(){
-    requestAnimationFrame(animate);
-    renderer.render(scene,camera);
-}
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-animate();
+    const line = new THREE.Line(geometry,material);
+    scene.add(line);
+
+    camera.position.z = 20;
+
+    function animate(){
+        requestAnimationFrame(animate);
+        renderer.render(scene,camera);
+    }
+
+    animate();
+
+});
 
