24805b498934ccbf97c625c0d940bf6a58feaa79
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-15 09:31:20 -0600

    append child

--- control23pLOT.js
+++ control23pLOT.js
@@ -1,38 +1,6 @@
+const renderer = new THREE.WebGLRenderer();
 
+renderer.setSize(window.innerWidth,window.innerHeight);
 
-document.addEventListener("DOMContentLoaded",()=>{
-
-    const scene = new THREE.Scene(); //Creating an abject scene
-    const camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-    ); // Creacting an object camera
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    const material = new THREE.LineBasicMaterial({color:0x0000ff})
-
-    const points = [];
-    points.push(new THREE.Vector3(-10,0,0));
-    points.push(new THREE.Vector3(10,8,10));
-
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-    const line = new THREE.Line(geometry,material);
-    scene.add(line);
-
-    camera.position.z = 20;
-
-    function animate(){
-        requestAnimationFrame(animate);
-        renderer.render(scene,camera);
-    }
-
-    animate();
-
-});
+document.body.appendChild(renderer.domElement);
 
